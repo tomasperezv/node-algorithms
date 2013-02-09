4f465865e16514fa45d5345b87be20478e35a477
Tomas Perez <tom@0x101.com>
2013-02-09 17:40:41 +0400

    Adding first base dependencies.

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp ./problems/example.js ./problems/$1
 sed -i "s/Example/$2/g" ./problems/$1
-sed -i "7i require('./problems/$1');" runner.js
+sed -i "10i require('./problems/$1');" runner.js
 echo "console.log(new $2().solve().check().toString());" >> runner.js
