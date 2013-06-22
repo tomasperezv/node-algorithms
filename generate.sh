2f02cc6b59ee5a4b7eab2c37df65dfceb41d2bdb
Tomas Perez <tom@0x101.com>
2013-06-23 00:49:07 +0400

    New structure of the files.
    	- runner script now placed under the problems folder.
    	- runner script in root folder now is just a basic example.
    	- Adapting generate.sh to be compatible.

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp ./problems/example.js ./problems/$1
+cp ./example/example.js ./problems/$1
 sed -i "s/Example/$2/g" ./problems/$1
-sed -i "10i require('./problems/$1');" runner.js
-echo "console.log(new $2().solve().check().toString());" >> runner.js
+sed -i "10i require('./$1');" ./problems/runner.js
+echo "console.log(new $2().solve().check().toString());" >> ./problems/runner.js
--- runner.js
+++ runner.js
@@ -6,6 +6,7 @@
 
 require('./lib/algorithm');
 require('./lib/value');
-require('./problems/example.js');
 
-console.log(new Example().solve(1000, [3, 5]).toString());
+require('./problems/multiples.js');
+
+console.log(new Multiples().solve(1000, [3, 5]).check(233168).toString());
