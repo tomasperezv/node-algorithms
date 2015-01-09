a386772935daf5ed35217abd57362a5089e9d885
Tomas Perez <tom@0x101.com>
2015-01-09 17:32:33 +0100

    Updating example.

--- example/example.js
+++ example/example.js
@@ -1,9 +1,10 @@
+var Algorithm = require('../lib/algorithm');
+
 /**
  * @class Example
  */
-
 Example = function() {
-	Algorithm.call(this);
+  Algorithm.call(this);
 };
 
 Example.prototype = new Algorithm();
@@ -13,30 +14,29 @@ Example.prototype = new Algorithm();
  * @method solve
  * @public
  */
-
 Example.prototype.solve = function(limit) {
 
-	this.resetInterval();
+  this.resetInterval();
 
-	var self = this;
+  var self = this;
 
-	var multiples = [];
+  var multiples = [];
 
-	this.iterate({
-		start: 1,
-		limit: limit
-	}).onEach(function(current) {
-		if (current.isMultiple(multipleOf)) {
-			multiples.push(current.value);
-		}
-	});
+  this.iterate({
+    start: 1,
+    limit: limit
+  }).onEach(function(current) {
+    if (current.isMultiple()) {
+      multiples.push(current.value);
+    }
+  });
 
-	// Now we need to calculate the sum
-	var totalSum = 0;
+  // Now we need to calculate the sum
+  var totalSum = 0;
 
-	this.iterate(multiples).onEach(function(current) {
-		totalSum += current.value;
-	});
+  this.iterate(multiples).onEach(function(current) {
+    totalSum += current.value;
+  });
 
   return this;
 
